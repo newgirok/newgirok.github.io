fcbf6e56e53261e53414630b57fc22e038f6ca6c
newgirok <newgirok@gmail.com>
2026-06-10 14:31:13 +0900

    fix: 컨테이너 너비 수정 및 헤더 jvns.ca 스타일 보완
    - flex child width 수축 버그 수정 (width: 100% 추가)
    - 헤더 구분선 오렌지 색상 적용
    - 2번째 네비 행 TIL ★ POSTS ★ LINKS 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,12 +6,20 @@ export default function Header() {
       <div className="header-top">
         <Link href="/" className="header-logo">newgirok</Link>
         <ul className="header-nav-primary">
-          <li><Link href="/til">TIL</Link></li>
           <li><Link href="/posts">POSTS</Link></li>
           <li><Link href="/links">LINKS</Link></li>
           <li><Link href="/about">ABOUT</Link></li>
         </ul>
       </div>
+      <nav className="header-nav-secondary-wrap">
+        <ul className="header-nav-secondary">
+          <li><Link href="/til">TIL</Link></li>
+          <li><span className="header-nav-sep">★</span></li>
+          <li><Link href="/posts">POSTS</Link></li>
+          <li><span className="header-nav-sep">★</span></li>
+          <li><Link href="/links">LINKS</Link></li>
+        </ul>
+      </nav>
       <hr className="header-divider" />
     </header>
   )
